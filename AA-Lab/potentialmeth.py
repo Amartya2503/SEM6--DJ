0950967760189dfa2cd0312bbe6e1a2f33085aa1
Amartya2503 <amartyamishra2503@gmail.com>
2024-05-12 18:56:31 +0530

    updated potential method

--- AA-Lab/potentialmeth.py
+++ AA-Lab/potentialmeth.py
@@ -7,14 +7,33 @@ def countones(a):
         if b&1:
             count+= 1
         b = b>>1
-    # print(f'the number of ones in {a} is {count}')
     return count
 
+def lisbin(a):
+    b = a
+    ls = []
+    while b:
+        ls.append(b&1)
+        b = b>>1
+    ls = ls[::-1]
+    ls = [0]*(4-len(ls)) +ls
+    return ls
+
+def diff(list1, list2):
+    cost = 0
+    for i in range(len(list1)):
+        if list1[i] != list2[i]:
+            cost += 1
+    return cost
+
 a = int(input("enter the value till which you want to count"))
-cost=0
-print(f'counter\t cost\t potential\t prevPotential\t totalcost')
+print(f'counter\t\t value\t\t actualcost\tpotentialDiff\t totalcost')
+pastlis = [0]*4
 for i in range(1,a):
     curpotential = countones(i)
     prevpotential = countones(i-1)
-    cost += 1+(curpotential - prevpotential)
-    print(f'{i}\t  {1}\t  {curpotential}\t\t  {prevpotential}\t\t  {cost}')
+    potentialDiff = curpotential - prevpotential
+    val = lisbin(i)
+    actual = diff(val, pastlis)
+    print(f'{i}\t  {val}\t\t  {actual}\t\t  {potentialDiff}\t\t  {actual + potentialDiff}')
+    pastlis = val
--- AA-Lab/test.py
+++ AA-Lab/test.py
@@ -28,57 +28,70 @@
                 #KD Tree#
 ############################################################################
 
-class TreeNode:
-    def __init__(self,val = None):
-        self.val = val
-        self.right = None
-        self.left = None
+# class TreeNode:
+#     def __init__(self,val = None):
+#         self.val = val
+#         self.right = None
+#         self.left = None
     
-def insertion(head,k):
-    print("enter the elements of node")
-    val = [0]*k
-    for i in range(k):
-        val[i] = int(input())
+# def insertion(head,k):
+#     print("enter the elements of node")
+#     val = [0]*k
+#     for i in range(k):
+#         val[i] = int(input())
         
-    newNode = TreeNode(val)
-    temp = head
-    if not temp:
-        head = newNode
-        return head
-    depth = 0
-    while(temp):
-        compindex = depth%k
-        if val[compindex] >= temp.val[compindex] :
-            print("moving right at depth ", depth)
-            if temp.right:
-                temp = temp.right
-            else:
-                print(f'inserting at depth {depth+1}')
-                temp.right = newNode
-                return newNode
-        else:
-            print("moving left at depth ", depth)
-            if temp.left:
-                temp = temp.left
-            else:
-                print(f'inserting at depth {depth+1}')
-                temp.left = newNode
-                return newNode
-        depth += 1
-def kdTree():
-    head = None
-    option = 0
-    k = int(input('enter the number of dimensions '))
-    while(option != -1):
-        option = int(input("enter the option "))
-        if option == 1:
-            if not head:
-                head = insertion(head,k)
-            else :
-                insertion(head, k)
+#     newNode = TreeNode(val)
+#     temp = head
+#     if not temp:
+#         head = newNode
+#         return head
+#     depth = 0
+#     while(temp):
+#         compindex = depth%k
+#         if val[compindex] >= temp.val[compindex] :
+#             print("moving right at depth ", depth)
+#             if temp.right:
+#                 temp = temp.right
+#             else:
+#                 print(f'inserting at depth {depth+1}')
+#                 temp.right = newNode
+#                 return newNode
+#         else:
+#             print("moving left at depth ", depth)
+#             if temp.left:
+#                 temp = temp.left
+#             else:
+#                 print(f'inserting at depth {depth+1}')
+#                 temp.left = newNode
+#                 return newNode
+#         depth += 1
+# def kdTree():
+#     head = None
+#     option = 0
+#     k = int(input('enter the number of dimensions '))
+#     while(option != -1):
+#         option = int(input("enter the option "))
+#         if option == 1:
+#             if not head:
+#                 head = insertion(head,k)
+#             else :
+#                 insertion(head, k)
         
-kdTree()
+# kdTree()
 
+
+###############################################################################
+                            #grahams scan
+###############################################################################
+
+##Steps of grahams scan algo :
+# 1. find the starting point which is min in coordinates
+# 2. sort the points in polar angles basis using atan2(points[0] -start[0], points[1]-start[1])
+# 3. define a function to find direction , same , clock or anticlock orientation is calculated using cross product(p,q,r)
+# 4. now get a stack in which insert the first 2 elements for all the following elements while the size of stack is greater than 2 and the incoming point is not making an anticlock movement keep popping the top of stack
+# 5. at the end we are left with the elements present in the grahams scan algo
+# def grahams(points):
+    
                 
 
 
